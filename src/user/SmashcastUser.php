41ee5657cc60c1720e420104b2d90c50011e48c7
jens1o <jenshausdorf@gmail.com>
2017-05-17 13:28:51 +0200

    further improving

--- src/user/SmashcastUser.php
+++ src/user/SmashcastUser.php
@@ -149,7 +149,7 @@ class SmashcastUser extends AbstractModel {
      * @return bool
      */
     public function isAuthenticated(): bool {
-        return (isset($this->data->login) && $this->data->login === 'true') || isset($this->data->user_email);
+        return (isset($this->data->login) && $this->data->login === 'true') || isset($this->data->authToken) || isset($this->data->user_email);
     }
 
     /**

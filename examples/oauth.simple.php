bcd20512e7d165cf509970b3d0458e9233e6a159
jens1o <jenshausdorf@gmail.com>
2017-05-20 12:01:52 +0200

    add some notes

--- examples/oauth.simple.php
+++ examples/oauth.simple.php
@@ -8,6 +8,7 @@ SmashcastApi::setAppName(APP_NAME);
 SmashcastApi::setAppToken(APP_TOKEN);
 SmashcastApi::setAppSecret(APP_SECRET);
 if(isset($_GET['authToken'])) {
+    // IMPORTANT NOTE: THIS IS VULNERABLE FOR XSS! YOU SHOULD ALWAYS ESCAPE THEM!
     echo 'Auth Token: ' . $_GET['authToken'];
     echo '<br><a href="' . $_SERVER['SCRIPT_NAME'] . '">Refresh!</a>';
 } else if(!isset($_GET['request_token'])) {
@@ -15,6 +16,7 @@ if(isset($_GET['authToken'])) {
     $forceAuth = true; // toggle this around and see what happens!
     SmashcastOauthHandler::init($forceAuth, 'somestatevalue');
 } else {
+    // IMPORTANT NOTE: THIS IS VULNERABLE FOR XSS! YOU SHOULD ALWAYS ESCAPE THEM!
     echo 'Request Token: ' . $_GET['request_token'];
     echo '<br>Auth Token: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' . SmashcastOauthHandler::getAuthTokenFromRequestToken($_GET['request_token']);
     echo '<br><a href="' . $_SERVER['SCRIPT_NAME'] . '">Refresh!</a>';

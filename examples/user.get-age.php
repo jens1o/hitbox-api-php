7a6589283c6b3e406e3df1a6841848e2f8cb6969
jens1o <jenshausdorf@gmail.com>
2017-05-27 12:30:38 +0200

    improve example

--- examples/user.get-age.php
+++ examples/user.get-age.php
@@ -5,6 +5,7 @@ use jens1o\smashcast\media\live\SmashcastLiveMedia;
 
 $liveMedia = new SmashcastLiveMedia('jens1o');
 
-$age = $liveMedia->getTimeCreated()->diff(new DateTime/* diff to now */)->format('%y years, %m months, %d days');
+$diff = $liveMedia->getTimeCreated()->diff(new DateTime/* diff to now */);
+$age = $diff->format('%y years, %m months, %d days');
 
 echo 'The channel is ' . $age . ' old';
